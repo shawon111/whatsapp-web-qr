2d79d60c3a2573ecd958b1d6653679e145a1b6c8
shawon111 <shawonahmmed08509@gmail.com>
2026-03-09 01:42:53 +0600

    apiKeyAuth middleware created

---
+++ src/middlewares/apiKeyAuth.js
@@ -0,0 +1,10 @@
+const apiKeyAuth = (req, res, next) =>{
+    const apiKey = req.headers['auth-api-key'];
+    if (!apiKey) {
+        return res.status(401).json({ message: 'API key is missing' });
+    }
+    if (!apiKey || apiKey !== process.env.AUTH_API_KEY) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    next();
+}
